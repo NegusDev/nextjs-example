c899734a87ac2897623723d1e52968bc0ba22a2c
NegusDev <enochnegus@gmail.com>
2024-06-03 11:47:26 +0300

    :tada: Initial commit

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function blog() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h2>THIS IS A BLOG</h2>
+        </main>
+    );
+}
